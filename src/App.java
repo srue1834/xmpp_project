be35d80a91b0d879d58b7ebe82a356df5abeb2c5
srue1834 <53914141+srue1834@users.noreply.github.com>
2022-08-15 21:55:21 -0600

    menu implementado

---
+++ src/App.java
@@ -0,0 +1,17 @@
+// sssrue18
+// zeus123
+
+// sssrue34
+// zeusxd
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        // Client client = new Client();
+        Menu menu = new Menu();
+        // client.connection_cofig();
+        menu.mainMenu();
+   
+    
+    }
+
+}
--- src/Client.java
+++ src/Client.java
@@ -3,66 +3,96 @@
  * 
  */
 
+ // Client client = new Client();
+// client.connection_cofig();
+
 import java.util.Scanner;
 
+import org.jivesoftware.smack.AccountManager;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 
 public class Client {
-    public Client() {
+    Scanner sc = new Scanner(System.in);
+
+    public XMPPConnection connection_cofig() throws Exception {
+        // connection object
+        ConnectionConfiguration config = new ConnectionConfiguration("alumchat.fun", 5222);
+        config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
+        config.setDebuggerEnabled(false);
+        config.setSendPresence(true);
+        
+        XMPPConnection con = new XMPPConnection(config);
+        con.connect();
+        
+        System.out.println("Connected to server\n");
+
+        return con;
     }
 
-    public void connection_cofig() throws Exception {
-        // create a thread that runs the connection to server
-        new Thread() {
-            public void run() {
-                try {
-                    // connection object
-                    ConnectionConfiguration config = new ConnectionConfiguration("alumchat.fun", 5222);
-                    config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
-                    config.setDebuggerEnabled(false);
-                    config.setSendPresence(true);
-                    XMPPConnection con = new XMPPConnection(config);
-                    con.connect();
-                    
 
-                    // create account manager for connection
-                    // AccountManager manager =  con.getAccountManager();
-                    // manager.createAccount("pruebauser2", "pass");
+    public void createAccount(XMPPConnection con) throws XMPPException {
 
-                    // login
-                    con.login("pruebauser", "pass");
+        // registration menu
+        System.out.println("\n+            Register a new account on the server             +\n");
+        System.out.print("Enter username: ");
+        String new_username = sc.nextLine();
+        System.out.print("\nEnter password: ");
+        String new_password = sc.nextLine();
 
-                    // class chat in smack lib
-                    Chat chat = con.getChatManager().createChat("pruebauser@alumchat.fun", new MessageListener() {
+        // create account manager for connection
+        AccountManager manager =  con.getAccountManager();
+        manager.createAccount(new_username, new_password);
+        // manager.createAccount("pruebauser2", "pass");
+    }
 
-                        @Override
-                        // method called each time a message is received
-                        public void processMessage(Chat chat, Message msg) {
-                            System.out.println(chat.getParticipant() + " said: " + msg.getBody());
+    public String login(XMPPConnection con) throws XMPPException {
+        
+        // login menu
+        System.out.println("\n+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+");
+        System.out.println("\n+                   Sign in with an account                   +\n");
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+        System.out.print("\nEnter password: ");
+        String password = sc.nextLine();
+        
+        // login
+        con.login(username, password);
+        return username;
+    }
 
-                            
-                        }
-                        
-                    });
+    public void logout(XMPPConnection con) throws XMPPException {
+        System.out.println("\nLogin out ...");
+        con.disconnect();
+        System.out.println("\nLogged out");
+    }
 
-                    System.out.println("Connected babyyyy");
-                    try (
-                    Scanner reader = new Scanner(System.in)) {
-                        while(con.isConnected()) {
-                            chat.sendMessage(reader.nextLine());
-                        }
-                    }
-
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
+    public void chat(XMPPConnection con) throws XMPPException {
+        // chat menu 
+        System.out.println("\n+                       Private chatroom                       +\n");
+        System.out.print("Enter username of the contact you want to chat with: ");
+        String contactName = sc.nextLine();
+        // class chat in smack lib "pruebauser@alumchat.fun"
+        Chat chat = con.getChatManager().createChat(contactName, new MessageListener() {
+            
+            @Override
+            // method called each time a message is received
+            public void processMessage(Chat chat, Message msg) {
+                System.out.println(chat.getParticipant() + " said: " + msg.getBody());
             }
-
-        }.start();
+        });
+        
+        while(con.isConnected()) {
+            System.out.print("\n>> ");
+            chat.sendMessage(sc.nextLine());
+        }
     }
+
+
+
     
 }
